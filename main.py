415b55306e83309b5d50cda9e655f84b20f3f57b
Frank Casanova <frankcasanova.info@gmail.com>
2023-10-03 17:59:14 +0200

    docstring added, and some refactor added

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import pyttsx3
 import speech_recognition as sr
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+def initialize_text_to_speech():
+    """
+    Inicializa el motor de texto a voz y configura la voz.
+    """
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    return engine
 
-def speak(audio):
-    engine.say(audio)
+def speak(text: str, engine):
+    """
+    Convierte el texto en voz y lo reproduce.
+
+    :param text: El texto que se convertirá en voz.
+    :param engine: El motor de texto a voz inicializado.
+    """
+    engine.say(text)
     engine.runAndWait()
 
-def get():
-    r = sr.Recognizer()
+def recognize_speech():
+    """
+    Reconoce el discurso del usuario y guarda el resultado en 'output.txt'.
+    """
+    recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print('Say something')
-        audio = r.listen(source)
-        print('done')
+        print('Di algo...')
+        audio = recognizer.listen(source)
+        print('Grabación terminada.')
 
         try:
-            text = r.recognize_google(audio)
-            print('Google thinks you said:\n' + text)
+            text = recognizer.recognize_google(audio, language="es-ES")
+            print('Google piensa que dijiste:\n' + text)
 
-            remember = open('output.txt', 'w')
-            remember.write(text)
-            remember.close()
+            with open('output.txt', 'w') as file:
+                file.write(text)
 
-        except Exception as e:
-            print(e)
+        except sr.UnknownValueError:
+            print('No se pudo entender el discurso.')
+        except sr.RequestError as e:
+            print(f'Error en la solicitud a Google: {e}')
 
-get()
+if __name__ == "__main__":
+    text_to_speech_engine = initialize_text_to_speech()
+    recognize_speech()
